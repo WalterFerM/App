eaffc6540e91870385f94e5a28d4871325cc5a5f
WalterFerM <wffmen@gmail.com>
2022-04-27 17:20:37 -0500

    styles-card-Temp-Search

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Card from './Card';
+import Styles from './Cards.module.css';
 
 export default function Cards({cities}) {
   return (
-  <div>
+  <div className={Styles.Cards}>
     {cities.map((city) => 
       <Card
         key={city.id}
-        min={city.main.min}
-        max={city.main.max}
+        min={city.main.temp_min}
+        max={city.main.temp_max}
         name={city.name}
         img={city.weather[0].icon}
         onClose={()=> alert (city.name)}
