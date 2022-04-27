b0cdb31b6c9e3fff8b753a6dec8c6f8fda7a5b5c
WalterFerM <wffmen@gmail.com>
2022-04-27 11:27:40 -0500

    component Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-export default function Card(props) {
-  // acá va tu código
-  return <div>Card Component</div>
+export default function Card({max, min, name, img, onClose}) {
+
+  function handleOnClose () {
+    if(typeof onClose === 'function') onClose();
+  }
+  return (
+  <div>
+    <button onClose={handleOnClose}>X</button>
+    <h1>{name}</h1>
+    <label htmlFor="">Min</label>
+    <span>{min}</span>
+    <label htmlFor="">Max</label>
+    <span>{max}</span>
+    <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="weather icon" />
+  </div>
+  )
 };
